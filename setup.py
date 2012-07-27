65340f28d76d4d38a6f0a40716b9f5f5d0860d08
Alon Horev <alonho@gmail.com>
2012-07-27 07:56:08 +0300

    add setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='mongoq',
+      version='0.1',
+      classifiers = ["Development Status :: 4 - Beta",
+                     "Intended Audience :: Developers",
+                     "License :: OSI Approved :: BSD License",
+                     "Programming Language :: Python :: 2"
+                     "Programming Language :: Python :: 3"],
+      description="mongoq generates MongoDB queries from simple python expressions",
+      author='Alon Horev',
+      author_email='alonho@gmail.com',
+      packages=['mongoq'],
+      license='BSD',
+      url='https://github.com/alonho/mongoq')
